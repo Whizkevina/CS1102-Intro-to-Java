4a30337ae55db19814b0089038643fb8919d5353
whizkevina <wizkevinaaina@gmail.com>
2020-07-14 23:06:43 +0100

    added MultipleChoiceQuestion constructor with Quiz

--- src/Quiz.java
+++ src/Quiz.java
@@ -7,40 +7,72 @@
  *
  */
 
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 public class Quiz {
 
 	/**
 	 * @param args
 	 */
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MultipleChoiceQuestion question = new MultipleChoiceQuestion(
+		"Who is the President of USA?",
+		"Barrack Obama",
+		"George Bush",
+		"Bill Clinton",
+		"Donald Trump",
+		"Abraham Lincoln",
+		"D");
+		question.check();
 		
-		String question = "What is the name of US Current President? \n";
 		
-		question += "A. George Bush \n";
-		question += "B. Barrack Obama \n";
-		question += "C. Kennedy J. F. \n";
-		question += "D. Hillary Clinton \n";
-		question += "E. Donald Trump \n";
+		MultipleChoiceQuestion question2 = new MultipleChoiceQuestion(
+		"What is the name of Superman?",
+		"Steve Clark",
+		"Kent Clark",
+		"Clark Kent",
+		"Ark Ent",
+		"Smallville",
+		"C");
+		question2.check();
 		
-		while (true) {
-			String answer = JOptionPane.showInputDialog(question);
-			
-			answer = answer.toUpperCase();
-			
-			if (answer.equals("E")) {
-				JOptionPane.showMessageDialog(null, "Correct!");
-				break;
-			} else if (answer.equals("A") || answer.equals("B") || answer.equals("C") || answer.equals("D")) {
-				JOptionPane.showMessageDialog(null, "Incorrect. Please try again");
-			} else {
-				JOptionPane.showMessageDialog(null, "Invalid answer. Please enter A, B, C, D or E.");
-			}
-			}
+		
+		MultipleChoiceQuestion question3 = new MultipleChoiceQuestion(
+		"Is Java the same as Javascript?",
+		"True",
+		"False",
+		"Not sure",
+		"Are you Kidding me",
+		"Uhmm, maybe",
+		"B");
+		question3.check();
+		
+		MultipleChoiceQuestion question4 = new MultipleChoiceQuestion(
+		"What is the Full meaning of U.S.A?",
+		"United State of Arab",
+		"United State of Asylum",
+		"United State of America",
+		"United State of Africa",
+		"USA",
+		"C");
+		question4.check();
+		
+		MultipleChoiceQuestion question5 = new MultipleChoiceQuestion(
+		"Is Java Object-oriented Programming language?",
+		"True",
+		"False",
+		"Not sure",
+		"Are you Kidding me",
+		"Uhmm, maybe",
+		"A");
+		question5.check();
+		
+
+		MultipleChoiceQuestion.showResults();
+		}
+	
 		}
 		
-		
-		
-			}
